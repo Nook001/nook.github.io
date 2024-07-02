36e63b35fdca8702effa56e5af3c9cb0c0b3a9ef
Nook <119006921+Nook001@users.noreply.github.com>
2024-07-02 15:23:07 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,20 +1,18 @@
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        const aboutP = entry.target.querySelector('.about p');
+        const aboutP = entry.target.querySelector('.aboutRight');
         const aboutL = entry.target.querySelector('.aboutLeft');
 
 
         if (entry.isIntersecting) {
             aboutL.classList.add('about-l-animation');
-            aboutP.classList.add('about-p-animation');
+            aboutP.classList.add('about-r-animation');
             return; // if we added the class, exit the function
         }
 
         // We're not intersecting, so remove the class!
         aboutL.classList.remove('about-l-animation');
-        aboutP.classList.remove('about-p-animation');
-
+        aboutP.classList.remove('about-r-animation');
     });
 });
-
 observer.observe(document.querySelector('.about'));
